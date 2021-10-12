1859df9285c4cf6843d50bca97c4656155e696a5
Adam Heeps <adamheeps@adams-mbp.home>
2021-10-12 20:17:17 +0100

    Added support to connect to mongoDB. Connection successful

--- src/server/index.js
+++ src/server/index.js
@@ -4,11 +4,15 @@
 // I don't think the server refreshes if changes are made...
 
 const express = require('express')
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+
 const app = express()
 const port = 3001
+const db = "mongodb://localhost/";
 
 app.use(function (req, res, next) {
-
+  // Authorizing API call to come from React front end on port 3000
   res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
   res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
@@ -24,3 +28,7 @@ app.get('/api', (req, res) => {
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
+
+mongoose.connect(db, {useNewUrlParser: true})
+    .then(() => console.log("Mongo connected"))
+    .catch(err => console.log(err));
