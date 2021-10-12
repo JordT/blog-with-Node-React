b6c3c402084d2b121adc0594a8ca66819738e347
Adam Heeps <adamheeps@adams-mbp.home>
2021-10-12 20:51:32 +0100

    Can now post database entries on the blogger schema

--- src/server/index.js
+++ src/server/index.js
@@ -6,10 +6,11 @@
 const express = require('express')
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
+const Blogger = require("/Users/adamheeps/development/blog-with-Node-React/src/models/Blogger.js");
 
 const app = express()
 const port = 3001
-const db = "mongodb://localhost/";
+const db = "mongodb://localhost/blogdb";
 
 app.use(function (req, res, next) {
   // Authorizing API call to come from React front end on port 3000
@@ -21,14 +22,40 @@ app.use(function (req, res, next) {
   next();
 });
 
-app.get('/api', (req, res) => {
-  res.send({response : 'Hello World!'})
-})
+app.use(
+  bodyParser.urlencoded({
+      extended: false
+  })
+);
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-})
+app.use(bodyParser.json());
 
 mongoose.connect(db, {useNewUrlParser: true})
     .then(() => console.log("Mongo connected"))
     .catch(err => console.log(err));
+
+app.get('/api', (req, res) => {
+  res.send({response : 'Hello World!'})
+})
+
+app.post("/blogger", (req, res) => {
+
+  const newBlogger = new Blogger({
+      name: req.body.name,
+      city: req.body.city
+  });
+
+Blogger.create(newBlogger)
+  .then(function(db) {
+      console.log(db);
+      res.json(db);
+  })
+  .catch(function(err) {
+      console.log(err);
+      res.json(err);
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
