46bec50d1fa8cd78a0359df9ec860e1c09c9c948
JordT <jordanthomson@Zoes-MacBook-Pro-2.local>
2021-10-12 17:24:37 +0100

    concurrently now running client and server together

--- src/server/index.js
+++ src/server/index.js
@@ -1,11 +1,11 @@
 // Simple server that returns hello world object on port 3005
 // cd into server folder and run 'node index.js' to begin.
-// navigate to http://localhost:3005/api
+// navigate to http://localhost:3001/api
 // I don't think the server refreshes if changes are made...
 
 const express = require('express')
 const app = express()
-const port = 3005
+const port = 3001
 
 app.get('/api', (req, res) => {
   res.send({response : 'Hello World!'})
