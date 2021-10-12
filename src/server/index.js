c5655159c467c5141768063750b2be3913cd1a8a
JordT <jordanthomson@Zoes-MacBook-Pro-2.local>
2021-10-12 11:42:05 +0100

    added link to working api

--- src/server/index.js
+++ src/server/index.js
@@ -1,5 +1,6 @@
 // Simple server that returns hello world object on port 3005
 // cd into server folder and run 'node index.js' to begin.
+// navigate to http://localhost:3005/api
 // I don't think the server refreshes if changes are made...
 
 const express = require('express')
