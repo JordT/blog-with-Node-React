d136c3447e411fb827a902fe93314a9ada112ee5
JordT <jordanthomson@Zoes-MacBook-Pro-2.local>
2021-10-15 16:32:08 +0100

    added menu drop down for small screen devices

--- src/client/components/Toolbar/Toolbar.js
+++ src/client/components/Toolbar/Toolbar.js
@@ -4,9 +4,10 @@ import './Toolbar.css'
 const Toolbar = () => {
     return (
         <div className="ToolbarContainer">
-            <h1 className="h1">Home</h1>
-            <h1 className="h1">About</h1>
-            <h1 className="h1">Blog Entries</h1>
+            <div class="Dropdown-menu">Menu</div>
+                <h1 className="h1">Home</h1>
+                <h1 className="h1">About</h1>
+                <h1 className="h1">Blog Entries</h1>
         </div>
     )
 }
