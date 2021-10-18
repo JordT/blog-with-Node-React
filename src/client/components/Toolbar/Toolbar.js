bf87162e348ec901a00c469d01c68591004eb52c
JordT <jordanthomson@Zoes-MacBook-Pro-2.local>
2021-10-18 16:39:22 +0100

    installed react-router-dom

--- src/client/components/Toolbar/Toolbar.js
+++ src/client/components/Toolbar/Toolbar.js
@@ -1,12 +1,13 @@
 import React from "react";
 import './Toolbar.css'
+import about from '../../pages/about.js'
 
 const Toolbar = () => {
     return (
         <div className="ToolbarContainer">
             <div class="Dropdown-menu">Menu</div>
                 <h1 className="h1">Home</h1>
-                <h1 className="h1">About</h1>
+                <h1 className="h1" href={about}>About</h1>
                 <h1 className="h1">Blog Entries</h1>
         </div>
     )
