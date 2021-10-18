269ef2d5960263cbf6510d1cd4c1993700385ea1
JordT <jordanthomson@Zoes-MacBook-Pro-2.local>
2021-10-18 16:56:23 +0100

    about route created but not linked to button

--- src/client/components/Toolbar/Toolbar.js
+++ src/client/components/Toolbar/Toolbar.js
@@ -7,7 +7,7 @@ const Toolbar = () => {
         <div className="ToolbarContainer">
             <div class="Dropdown-menu">Menu</div>
                 <h1 className="h1">Home</h1>
-                <h1 className="h1" href={about}>About</h1>
+                <h1 className="h1">About</h1>
                 <h1 className="h1">Blog Entries</h1>
         </div>
     )
