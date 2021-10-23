4aabcbedddf887c60b8146e1ebe2655a05a14228
JordT <jordanthomson@zoes-mbp-2.home>
2021-10-23 17:35:02 +0100

    Git configured - test files must start lower case

--- src/__tests__/app.test.js
+++ src/__tests__/app.test.js
@@ -1,1 +1,8 @@
-//dummy test file
+import { render, screen } from '@testing-library/react';
+import App from '../client/App';
+
+test('renders learn react link', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/learn react/i);
+  expect(linkElement).toBeInTheDocument();
+});
